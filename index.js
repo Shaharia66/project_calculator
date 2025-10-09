95a673f25acf21a7a764dd4d7202e92e12bcbad4
Shaharia66 <gssaif.tm@gmail.com>
2025-10-09 13:54:22 +0600

    day3

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// const btn1=document.querySelector(".btn1");
-// const int1=document.querySelector("#int1");
+const btn1=document.querySelector(".btn1");
+const int1=document.querySelector("#int1");
 
-// int1.addEventListener("click",display());
-// let display=(numb)=>{
-//     btn1.innerText +=numb;
-// }
-// const btn2=document.querySelector(".btn2");
+int1.addEventListener("click",display(btn1.value));
+let display=(numb)=>{
+    btn1.innerText +=numb;
+}
+const btn2=document.querySelector(".btn2");
 
-{let age = 6;
-console.log(age);}
-let age=5;
-console.log(age);
